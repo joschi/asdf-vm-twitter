2791f0ec0a9a76d18ddbf335a7ea8dc43a5ffa76
Jochen Schalanda <jochen@schalanda.name>
2022-12-12 22:41:52 +0100

    Handle removal of plugins

--- bin/update.bash
+++ bin/update.bash
@@ -20,18 +20,21 @@ ensure_dir "${DATA_DIR}"
 ensure_dir "${TWEETS_DIR}"
 ensure_dir "${TOOTS_DIR}"
 
-asdf plugin list all 2> /dev/null | awk '{ print $1 }' | sort > "${TEMP_DIR}/plugins-new.txt"
+# Initialize plugin repository
+asdf plugin list all > /dev/null 2>&1
+asdf plugin list all | awk '{ print $1 }' | sort > "${TEMP_DIR}/plugins-new.txt"
 if [[ ! -r "${DATA_DIR}/plugins.txt" ]]
 then
 	touch "${DATA_DIR}/plugins.txt"
 fi
 comm -13 "${DATA_DIR}/plugins.txt" "${TEMP_DIR}/plugins-new.txt" | sort > "${TEMP_DIR}/plugins-added.txt"
+comm -23 "${DATA_DIR}/plugins.txt" "${TEMP_DIR}/plugins-new.txt" | sort > "${TEMP_DIR}/plugins-removed.txt"
 
 while IFS= read -r plugin
 do
 	echo "Added plugin ${plugin}"
 
-	PLUGIN_REPO="$(sed -e 's/repository = //' "~/.asdf/repository/plugins/${plugin}")"
+	PLUGIN_REPO="$(sed -e 's/repository = //' "${HOME}/.asdf/repository/plugins/${plugin}")"
 
 	# Tweet new plugins
 	cat<<EOF > "${TWEETS_DIR}/plugin-${plugin}.tweet"
@@ -44,10 +47,9 @@ EOF
 	cp "${TWEETS_DIR}/plugin-${plugin}.tweet" "${TOOTS_DIR}/plugin-${plugin}.toot"
 done < "${TEMP_DIR}/plugins-added.txt"
 
-sort "${DATA_DIR}/plugins.txt" "${TEMP_DIR}/plugins-new.txt" | uniq > "${TEMP_DIR}/plugins-merged.txt"
-diff -u "${DATA_DIR}/plugins.txt" "${TEMP_DIR}/plugins-merged.txt" || true
-mv -f "${TEMP_DIR}/plugins-merged.txt" "${DATA_DIR}/plugins.txt"
-rm -f "${TEMP_DIR}/plugins-new.txt" "${TEMP_DIR}/plugins-added.txt"
+diff -u "${DATA_DIR}/plugins.txt" "${TEMP_DIR}/plugins-new.txt" || true
+mv -f "${TEMP_DIR}/plugins-new.txt" "${DATA_DIR}/plugins.txt"
+rm -f "${TEMP_DIR}/plugins-added.txt"
 
 parallel -a "${DATA_DIR}/plugins.txt" -j 4 "${BIN_DIR}"/update-plugin.bash
 
